cbd57cee9ab043913b1f29eb5d87900546ceb633
eennk <eennk@DESKTOP-8V8B7U4>
2022-12-01 22:05:21 +0400

    Add project files.

---
+++ Proxy_FlyWeight/FlyWeight.cs
@@ -0,0 +1,28 @@
+﻿//////////////////////////////// FlyWeight
+namespace Flyweight;
+
+interface Flyweight
+{
+    public void Operation(string Ammotype);
+}
+
+class ConcreteFlyweight : Flyweight
+{
+    public void Operation(string ammo)
+    {
+        Console.WriteLine(" Ammo type: " + ammo);
+    }
+}
+
+class FlyweightFactory
+{
+    private static Dictionary<string, Flyweight> _flyweights = new();
+    public FlyweightFactory()
+    {
+        _flyweights.Add("Pistol", new ConcreteFlyweight());
+        _flyweights.Add("AK-47", new ConcreteFlyweight());
+    }
+
+    public Flyweight GetFlyweight(string key) => _flyweights[key];     //return ammo type
+
+}
---
+++ Proxy_FlyWeight/Proxy.cs
@@ -0,0 +1,38 @@
+﻿//////////////////////////////// Proxy
+class Employee
+{
+    public string? Username { get; set; }
+    public string? Password { get; set; }
+    public string? Role { get; set; }
+}
+
+interface ISharedFolder
+{
+    void PerformRWOperations();
+}
+
+class SharedFolder : ISharedFolder
+{
+    public void PerformRWOperations()
+    {
+        // Some Code...
+    }
+}
+class SharedFolderProxy : ISharedFolder
+{
+    private ISharedFolder? folder;
+    private Employee? employee;
+    
+    public void PerformRWOperations()
+    {
+        //Does employee has access to the shared file
+        if (employee.Role == "fehle" || employee.Role == "patron")
+        {
+            folder = new SharedFolder();
+            Console.WriteLine("Obyekt Safe olundu");
+            folder.PerformRWOperations();
+        }
+        else
+            Console.WriteLine("Obyekt Safe olundu");
+    }
+}
